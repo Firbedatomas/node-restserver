7696fd7a0a869fb6b6cadce295c9a8c74c7db214
Tomas <firbedatomasf@gmail.com>
2020-09-30 15:03:35 -0300

    RestServer-usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,18 @@
+//=================
+//PUERTO
+//=================
 process.env.PORT = process.env.PORT || 3001;
+
+//=================
+//ENTORNO
+//=================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost/cafe';
+} else {
+    urlDB = 'mongodb+srv://firbeda:I0SeTRjyQKQcjUcZ@cluster0.bde3h.mongodb.net/cafe';
+
+}
+process.env.URLDB = urlDB;
